0e52dc4d38cdc27359c9a67e57b80b0af6b8175f
murathankose <murathankose@icloud.com>
2019-02-19 12:35:21 +0300

    Little change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,27 +1,4 @@
-//package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-/*import java.util.*;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-  public static boolean search(ArrayList<Integer> array, int e) {
-        System.out.println("inside search");
-        if (array == null) return false;
-  
-        for (int elt : array) {
-          if (elt == e) return true;
-        }
-        return false;
-      }
-}
-*/
 package com.mycompany.app;
 
 import static spark.Spark.get;
